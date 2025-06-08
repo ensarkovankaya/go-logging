3fb5cf233a404ece18df4dab65d3f4c469f38868
ensarkovankaya <ensar@kovankaya.com>
2025-06-08 15:59:12 +0300

    refactor request ID handling to use LogIDBuilder for improved logging

--- http/http.go
+++ http/http.go
@@ -11,9 +11,9 @@ import (
 const DefaultRequestIDHeader = "X-Request-ID"
 
 type Option func(*Transport)
-type RequestIDBuilder func(req *http.Request) string
+type LogIDBuilder func(req *http.Request) string
 
-var DefaultRequestIDBuilder RequestIDBuilder = func(req *http.Request) string {
+var DefaultLogIDBuilder LogIDBuilder = func(req *http.Request) string {
 	requestID := req.Header.Get(DefaultRequestIDHeader)
 	if requestID != "" {
 		return requestID
@@ -25,7 +25,7 @@ type Transport struct {
 	Serializer
 	Logger           core.Logger
 	Transport        http.RoundTripper
-	RequestIDBuilder RequestIDBuilder
+	RequestIDBuilder LogIDBuilder
 	NowFunc          func() time.Time
 }
 
@@ -34,7 +34,7 @@ func NewTransport(logger core.Logger, options ...Option) *Transport {
 		Serializer:       Serializer{},
 		Logger:           logger,
 		Transport:        http.DefaultTransport,
-		RequestIDBuilder: DefaultRequestIDBuilder,
+		RequestIDBuilder: DefaultLogIDBuilder,
 		NowFunc:          time.Now,
 	}
 	for _, opt := range options {
@@ -47,25 +47,25 @@ func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	if GetLoggingDisabled(req.Context()) {
 		return t.Transport.RoundTrip(req)
 	}
-	requestID := t.RequestIDBuilder(req)
+	id := t.RequestIDBuilder(req)
 	serializedRequest, err := t.Request(req)
 	if err != nil {
-		t.Logger.Error(req.Context(), "Failed to serialize request", core.F("requestID", requestID), core.E(err))
+		t.Logger.Error(req.Context(), "Failed to serialize request", core.F("id", id), core.E(err))
 		return nil, err
 	}
-	t.Logger.Info(req.Context(), "Request", core.F("requestID", requestID), core.F("request", serializedRequest))
+	t.Logger.Info(req.Context(), "Request", core.F("id", id), core.F("request", serializedRequest))
 	startTime := t.NowFunc()
 	response, err := t.Transport.RoundTrip(req)
 	elapsed := t.NowFunc().Sub(startTime)
 	if err != nil {
-		t.Logger.Error(req.Context(), "HTTP error", core.F("requestID", requestID), core.E(err))
+		t.Logger.Error(req.Context(), "HTTP error", core.F("id", id), core.E(err))
 		return nil, err
 	}
 	serializedResponse, err := t.Response(response)
 	if err != nil {
-		t.Logger.Error(req.Context(), "Failed to serialize response", core.F("requestID", requestID), core.E(err))
+		t.Logger.Error(req.Context(), "Failed to serialize response", core.F("id", id), core.E(err))
 		return nil, err
 	}
-	t.Logger.Info(req.Context(), "Response", core.F("requestID", requestID), core.F("response", serializedResponse), core.F("elapsed", elapsed))
+	t.Logger.Info(req.Context(), "Response", core.F("id", id), core.F("response", serializedResponse), core.F("elapsed", elapsed))
 	return response, nil
 }
