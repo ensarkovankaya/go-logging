aa4e5cf7f6ad61b13929449512c93ea8026899ef
ensarkovankaya <ensar@kovankaya.com>
2025-06-09 00:07:35 +0300

    add gorm integration

--- go.mod
+++ go.mod
@@ -7,16 +7,18 @@ require (
 	github.com/getsentry/sentry-go v0.33.0
 	github.com/google/uuid v1.6.0
 	go.uber.org/zap v1.27.0
+	gorm.io/gorm v1.30.0
 )
 
 require (
 	github.com/elastic/elastic-transport-go/v8 v8.7.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
 	go.opentelemetry.io/otel v1.28.0 // indirect
 	go.opentelemetry.io/otel/metric v1.28.0 // indirect
 	go.opentelemetry.io/otel/trace v1.28.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/text v0.25.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
 )
---
+++ integrations/gorm/logger.go
@@ -0,0 +1,41 @@
+package gorm
+
+import (
+	"context"
+	gormLogger "gorm.io/gorm/logger"
+	"time"
+
+	"github.com/ensarkovankaya/go-logging/core"
+)
+
+type Logger struct {
+	Logger core.Interface
+}
+
+func New(logger core.Interface) gormLogger.Interface {
+	return &Logger{
+		Logger: logger,
+	}
+}
+
+func (l *Logger) LogMode(_ gormLogger.LogLevel) gormLogger.Interface {
+	return l
+}
+
+func (l *Logger) Info(ctx context.Context, msg string, data ...interface{}) {
+	l.Logger.Info(ctx, msg, core.F("data", data))
+}
+
+func (l *Logger) Warn(ctx context.Context, msg string, data ...interface{}) {
+	l.Logger.Warning(ctx, msg, core.F("data", data))
+}
+
+func (l *Logger) Error(ctx context.Context, msg string, data ...interface{}) {
+	l.Logger.Error(ctx, msg, core.F("data", data))
+}
+
+func (l *Logger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
+	query, rowsAffected := fc()
+	elapsed := time.Since(begin)
+	l.Logger.Debug(ctx, "", core.F("elapsed", elapsed.Seconds()), core.F("query", query), core.F("rowsAffected", rowsAffected), core.E(err))
+}
