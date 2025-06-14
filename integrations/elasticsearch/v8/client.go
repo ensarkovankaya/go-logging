f721d51b4af26f43c2a7c9bf994982192725b50d
ensarkovankaya <ensar@kovankaya.com>
2025-06-14 19:49:28 +0300

    Add environment variable support for configuring number of workers in bulk indexer

--- integrations/elasticsearch/v8/client.go
+++ integrations/elasticsearch/v8/client.go
@@ -18,6 +18,7 @@ import (
 const (
 	envFlushBytes    = "ELASTICSEARCH_FLUSH_BYTES"
 	envFlushInterval = "ELASTICSEARCH_FLUSH_INTERVAL"
+	envNumWorkers    = "ELASTICSEARCH_NUM_WORKERS"
 )
 
 var (
@@ -25,6 +26,7 @@ var (
 	globalBulkIndexer esutil.BulkIndexer
 	flushBytes        = int(5e+6) // 5 MB
 	flushInterval     = 30 * time.Second
+	numWorkers        = runtime.NumCPU() // Default is the number of CPU cores
 )
 
 type (
@@ -69,7 +71,7 @@ func InitializeClient(options ...ClientOption) (*elasticsearch.Client, error) {
 func InitializeBulkIndexer(options ...BulkIndexerOption) (esutil.BulkIndexer, error) {
 	cfg := &esutil.BulkIndexerConfig{
 		Client:        globalClient,
-		NumWorkers:    runtime.NumCPU(),
+		NumWorkers:    numWorkers,
 		FlushBytes:    flushBytes,
 		FlushInterval: flushInterval,
 	}
@@ -126,6 +128,18 @@ func init() {
 				"Invalid %v value, using default %v\n",
 				envFlushBytes,
 				flushBytes,
+			)
+		}
+	}
+	if os.Getenv(envNumWorkers) != "" {
+		if workerCount, err := strconv.Atoi(os.Getenv(envNumWorkers)); err == nil && workerCount > 0 {
+			numWorkers = workerCount
+		} else {
+			_, _ = fmt.Fprintf(
+				os.Stderr,
+				"Invalid %v value, using default %v\n",
+				envNumWorkers,
+				numWorkers,
 			)
 		}
 	}
